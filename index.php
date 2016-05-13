90fd59dbfc0138ed519ba93003847b496f1dbaca
--replace-all <--replace-all>
2016-05-13 09:37:24 +0200

    open / created and open a text file for reading

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
- 
+
+//define relative path to source, which is our file
+$path = 'files/stock.txt';
+
+//attempt to open a file for reading
+$handle = fopen($path, 'r');
+
+//test if we have a successfully created a handle to our file
+if($handle){
+    "$path was successfully opened for reading";
+}else{
+    "failed to open $path for reading";
+}
